69281f57476fc11265bfe89d07810d4104d7b291
Ashutosh <bhardwajashu96@hotmail.com>
2020-05-05 10:23:37 +0530

    Add storybook config

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ["../stories/*.stories.js"],
+  addons: [
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-notes/register-panel",
+    "@storybook/addon-storysource",
+    "@storybook/addon-viewport/register",
+    "@storybook/addon-backgrounds",
+  ],
+};
