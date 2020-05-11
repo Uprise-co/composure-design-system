95eef8e7f9747561fb087575e3a84c0fdc5b76fe
Ashutosh <bhardwajashu96@hotmail.com>
2020-05-11 10:42:00 +0530

    fix: fix missing image issue and add storybook-deployer

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = async ({ config }) => {
   // styles
   config.module.rules.push({
     test: /\.(sass|scss)$/,
-    use: ['resolve-url-loader'],
-    include: path.resolve(__dirname, '../')
+    use: ["resolve-url-loader"],
+    include: path.resolve(__dirname, "../")
   });
   // fonts
   config.module.rules.push({
-    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+    test: /\.(woff|woff2|eot|ttf)$/,
     use: [
       {
-        loader: 'file-loader',
+        loader: "file-loader",
         query: {
-          name: '[name].[ext]'
+          name: "[name].[ext]"
         }
       }
     ],
-    include: path.resolve(__dirname, '../')
+    include: path.resolve(__dirname, "../")
   });
 
   return config;
